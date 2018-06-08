c297ac46f08634df55c4e90806e8c82abc342486
whoyanbing <whoyanbing@gmail.com>
2018-06-08 17:34:39 +0800

    at work

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
-
+#!/bin/bash
 all: Sqlite
 
 s.o: sqlite.cc sqlite.h
 	g++ -o s.c -c sqlite.cc
+t.o: test.cc sqlite.h
+	g++ -o t.o -c test.cc
 st.o: sqlite_test.cc sqlite.h
 	g++ -o st.c -c sqlite_test.cc
 at.o: Account_test.cc sqlite.h
 	g++ -o at.c -c Account_test.cc
 f.o: function.cc sqlite.h
 	g++ -o f.c -c function.cc
-Sqlite: s.o st.o at.o f.o
-	g++ -o Sqlite s.o st.o st.o f.o -lsqlite3
+Sqlite: s.o t.o st.o at.o f.o
+	g++ -o Sqlite s.o t.o st.o st.o f.o -lsqlite3
+
 .PHONY: clean
 clean:
-	rm -f s.o st.o at.o f.o Sqlite
+	rm -f s.o t.o st.o at.o f.o Sqlite
 
--- src/sqlite.cc
+++ src/sqlite.cc
@@ -1,35 +1,20 @@
 #include "sqlite.h"
 #include <iostream>
-<<<<<<< HEAD
 #include "function.cc"
-=======
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
 
 void SqStmt::bind(int index, int val)
 {
 	sqlite3_bind_int(stmt_, index, val);
 }
 
-<<<<<<< HEAD
-void SqStmt::bind(int index, const std::string& val)
-{
-    std::cout << val.c_str() <<  std::endl;
-	std::cout << sqlite3_bind_text(stmt_, index, val.c_str(), -1, SQLITE_STATIC) << std::endl;
-}
-
-void SqStmt::bind(int index, double val)
-{
-	sqlite3_bind_double(stmt_, index, val);
-=======
-void SqStmt::bind(int index, double val)
-{
-	sqlite3_bind_double(stmt_, index, val);
-}
-
 void SqStmt::bind(int index, const std::string& val)
 {
 	sqlite3_bind_text(stmt_, index, val.c_str(), -1, SQLITE_STATIC);
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
+}
+
+void SqStmt::bind(int index, double val)
+{
+	sqlite3_bind_double(stmt_, index, val);
 }
 
 int SqStmt::step()
@@ -37,26 +22,16 @@ int SqStmt::step()
 	return sqlite3_step(stmt_);
 }
 
-<<<<<<< HEAD
 int SqStmt::column_int(int n)
 {
     return sqlite3_column_int(stmt_,n);
-=======
-int SqStmt::column_int(int num)
-{
-	return sqlite3_column_int(stmt_, num);	
-}
-
-const char* SqStmt::column_txt(int num)
-{
-	return (const char*)sqlite3_column_text(stmt_, num);
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
 }
 
 const char* SqStmt::column_txt(int n)
 {
     return (const char*)sqlite3_column_text(stmt_,n);
 }
+
 SqStmt::~SqStmt()
 {
 	sqlite3_finalize(stmt_);
@@ -69,22 +44,15 @@ Sqlite::Sqlite(const std::string& dbname)
 
 SqStmt Sqlite::prepare(const std::string& sql)
 {
-<<<<<<< HEAD
     SqStmt stmt;
-=======
-	SqStmt stmt;
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
 	sqlite3_prepare_v2(db_, sql.c_str(), -1, &stmt.stmt_, NULL);
 	return stmt;
 }
 
 int Sqlite::exec(const std::string& sql)
 {
-<<<<<<< HEAD
 	return sqlite3_exec(db_, sql.c_str(), NULL,NULL,0);
-=======
 	sqlite3_exec(db_, sql.c_str(), NULL, NULL, 0);
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
 }
 
 Sqlite::~Sqlite()
@@ -92,7 +60,6 @@ Sqlite::~Sqlite()
 	sqlite3_close(db_);
 }
 
-<<<<<<< HEAD
 bool AccountDAO::isset_user(const std::string& name)
 {
     SqStmt st = sqlite_.prepare("select * from user where USERNAME=?");
@@ -205,15 +172,4 @@ long AccountDAO::balance(const std::string& name)
     if(st.step() == SQLITE_ROW){
         return st.column_int(3);
     }
-=======
-void AccountDAO::auth(const std::string& name, const std::string& pwd)
-{
-	SqStmt st = sqlite_.prepare("select * from user where username=? and password=?");
-	st.bind(1,name);
-	st.bind(2,pwd);
-	if(st.step() == SQLITE_ROW)
-		std::cout << "login success" << std::endl;
-	else
-		std::cout << "error" << std::endl;
->>>>>>> e69753ce7b3dbb6255efedcc917a50ad053876ce
 }
